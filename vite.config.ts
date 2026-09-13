ccafb2bf8fe6b40686169298b9193574912178d6
Philip Lewis <philiplewis@GD79Q13KHM.local>
2026-09-13 12:29:53 +0100

    Add empty plugins array for Cloudflare compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  plugins: [],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
